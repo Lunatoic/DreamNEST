e6f2ad65a5ccb0b86090d8f2d15da1a0a4f652a0
Nikhil <nikhildgr788@gmail.com>
2024-04-04 11:00:12 +0530

    Front-end of register page is complete and we started working on backend

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -3,46 +3,67 @@ import { useNavigate } from "react-router-dom"
 import "../styles/Register.scss";
 
 const RegisterPage = () => {
+
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+    profileImage: null,
+  });
+
+  const handleChange = (e) => {
+    const { name, value, files } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+      [name]: name === "profileImage" ? files[0] : value,
+    });
+  };
+
+  console.log(formData)
+
   return (
     <div className="register">
       <div className="register_content">
-        <form action="">
+        <form className="register_content_form">
 
         <input
             placeholder="First Name"
             name="firstName"
-            // value={formData.firstName}
-            // onChange={handleChange}
+            value={formData.firstName}
+            onChange={handleChange}
             required
           />
           <input
             placeholder="Last Name"
             name="lastName"
-            // value={formData.lastName}
-            // onChange={handleChange}
+            value={formData.lastName}
+            onChange={handleChange}
             required
           />
           <input
             placeholder="Email"
             name="email"
             type="email"
-            // value={formData.email}
-            // onChange={handleChange}
+            value={formData.email}
+            onChange={handleChange}
             required
           />
           <input
             placeholder="Password"
             name="password"
-            // value={formData.password}
-            // onChange={handleChange}
+            value={formData.password}
+            onChange={handleChange}
             type="password"
             required
           />
           <input
             placeholder="Confirm Password"
             name="confirmPassword"
-            // value={formData.confirmPassword}
-            // onChange={handleChange}
+            value={formData.confirmPassword}
+            onChange={handleChange}
             type="password"
             required
           />
@@ -52,12 +73,21 @@ const RegisterPage = () => {
           name="profileImage"
           accept="image/*"
           style={{display:"none"}}
+          onChange={handleChange}
           required
           />
           <label htmlFor="image">
             <img src="/assets/addImage.png" alt="add profile photo" />
             <p>Upload Your Photo</p>
           </label>
+
+          {formData.profileImage && (
+            <img
+              src={URL.createObjectURL(formData.profileImage)}
+              alt="profile photo"
+              style={{ maxWidth: "80px" }}
+            />
+          )}
 
           <button type="submit">REGISTER</button>
 
