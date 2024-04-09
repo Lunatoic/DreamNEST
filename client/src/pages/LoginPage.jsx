7c96af5cec62a57fb0b495a54bd1eb8e73dad5d1
Nikhil <nikhildgr788@gmail.com>
2024-04-09 17:58:16 +0530

    LogIn page front-end designed.

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
+import '../styles/Login.scss'
 
 const LoginPage = () => {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+
   return (
-    <div>
-      LogIn Page
+    <div className="login">
+      <div className="login_content">
+        <form className="login_content_form">
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+          <button type="submit">LOG IN</button>
+        </form>
+        <a href="/register">Don't have an account? Sign In Here</a>
+      </div>
     </div>
-  )
+  );
 }
 
 export default LoginPage
